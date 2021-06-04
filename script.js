d5f6926d12463212a8ed2b9854a6aa75ca98799b
Huu Nguyen <thieuvan0405@gmail.com>
2021-06-04 16:21:30 +0700

    Update description

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Developer", "Computer Scientist", "Data Scientist"],
+        strings: ["Student", "Developer", "Computer Scientist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Developer", "Computer Scientist", "Data Scientist"],
+        strings: ["Student", "Developer", "Computer Scientist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
